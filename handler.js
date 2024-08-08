c709c281d09d96871608e3a4f1a83fc0e5e9796f
karinagassanova <karinegassanova@gmail.com>
2024-08-08 17:09:38 +0100

    Add Numbers.

--- handler.js
+++ handler.js
@@ -55,6 +55,29 @@ module.exports.hello = (event, context, callback) => {
   callback(null, response);
 };
 
+// addNumbers endpoint
+module.exports.addNumbers = (event, context, callback) => {
+  const body = JSON.parse(event.body);  // Parse the incoming request body
+  const num1 = body.num1;
+  const num2 = body.num2;
+
+  // Calculate the sum of the two numbers
+  const sum = num1 + num2;
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      result: sum
+    }),
+  };
+
+  callback(null, response);
+};
 module.exports.createProduct = (event, context, callback) => {
 
   // Do work to create Product
