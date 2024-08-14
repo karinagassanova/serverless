46c49a0dbc061802f7363fd86110c364a30f1f65
karinagassanova <karinegassanova@gmail.com>
2024-08-14 09:30:35 +0100

    Webhook

--- handler.js
+++ handler.js
@@ -1,5 +1,61 @@
 'use strict';
+const https = require('https');
 
+module.exports.postToWebhook = (event, context, callback) => {
+  const postData = JSON.stringify({ key: 'value' }); // Customize the payload if needed
+
+  const options = {
+    hostname: 'webhook.site',
+    port: 443,
+    path: '/16231f4c-21bb-4de0-902e-62ed4de07610/test',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(postData)
+    }
+  };
+
+  const req = https.request(options, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          message: "POST request completed successfully",
+          result: JSON.parse(data)
+        }),
+      };
+
+      callback(null, response);
+    });
+  });
+
+  req.on('error', (e) => {
+    const response = {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: "Error occurred",
+        error: e.message
+      }),
+    };
+
+    callback(null, response);
+  });
+
+  req.write(postData);
+  req.end();
+};
 module.exports.getProduct = (event, context, callback) => {
 
   // Do work to retrieve Product
@@ -8,8 +64,6 @@ module.exports.getProduct = (event, context, callback) => {
   const response = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
@@ -26,8 +80,6 @@ module.exports.sample = (event, context, callback) => {
   const response = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
@@ -43,8 +95,6 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
@@ -67,8 +117,6 @@ module.exports.addNumbers = (event, context, callback) => {
   const response = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
@@ -78,6 +126,7 @@ module.exports.addNumbers = (event, context, callback) => {
 
   callback(null, response);
 };
+
 module.exports.createProduct = (event, context, callback) => {
 
   // Do work to create Product
@@ -86,8 +135,6 @@ module.exports.createProduct = (event, context, callback) => {
   const response = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
